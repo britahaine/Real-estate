6652f86c9922509e557e8aeaedb4493765fcf55f
britahaine <britahainembabazi23@gmail.com>
2026-07-07 15:56:43 +0300

    addend navbar for large screens and mobile screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-      <h1>Get started</h1>
-    </>
+    <div className="w-full overflow-hidden">
+     <Header />
+ 
+   
+    </div>
   );
 }
 
