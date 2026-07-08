a0dcc4e6a57316a38fef9d0a56024e546007832f
britahaine <britahainembabazi23@gmail.com>
2026-07-08 11:48:07 +0300

    added the projects session

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import About from "./components/About";
 import Header from "./components/Header";
+import Project from "./components/Project";
 
 function App() {
   return (
     <div className="w-full overflow-hidden">
      <Header />
      <About />
+     <Project />
  
    
     </div>
