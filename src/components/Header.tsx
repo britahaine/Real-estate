7506193d8408059fe3347089bb26532bd9df5ac2
britahaine <britahainembabazi23@gmail.com>
2026-07-14 09:37:29 +0300

    testing purposes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
       viewport={{ once: true }}
       className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
         <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20">
-          Explore homes that fit your dreams
+          Explore homes that fit your dreams with us.
         </h2>
 
         <div className="space-x-8 mt-16">
